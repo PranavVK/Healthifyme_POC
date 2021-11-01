eb2eb18a154d9503e67cec84dfa1237f2ededc5a
PranavVK <pranav.michu@gmail.com>
2021-11-01 18:21:40 +0530

    Changes for test data abstraction

--- src/test/java/com/healthifyme/tests/LoginTest.java
+++ src/test/java/com/healthifyme/tests/LoginTest.java
@@ -6,10 +6,33 @@ import com.healthifyme.pages.LoginPage;
 import io.appium.java_client.MobileElement;
 import org.testng.Assert;
 import org.testng.annotations.*;
+import org.json.JSONObject;
+import org.json.JSONTokener;
+
+import java.io.InputStream;
 
 public class LoginTest extends BaseTest {
     LoginPage loginPage;
     DashboardPage dashboardPage;
+
+    InputStream testData;
+    JSONObject loginUser;
+
+    @BeforeClass
+    public void beforeClass() throws Exception {
+        try {
+            String dataFileName = "TestData.json";
+            testData = getClass().getClassLoader().getResourceAsStream(dataFileName);
+            JSONTokener jsonTokener = new JSONTokener(testData);
+            loginUser = new JSONObject(jsonTokener);
+        } catch (Exception e) {
+            e.printStackTrace();
+        } finally {
+            if (testData != null) {
+                testData.close();
+            }
+        }
+    }
 
     @BeforeMethod
     public void beforeMethod() {
@@ -18,10 +41,13 @@ public class LoginTest extends BaseTest {
 
     @Test
     public void loginValidation() {
+        String userName = loginUser.getJSONObject("validUser").getString("username");
+        String password = loginUser.getJSONObject("validUser").getString("password");
+
         loginPage.navigateToLoginScreen();
         loginPage.tapOnLoginWithEmailButton();
-        loginPage.enterUserName("hme-testpr501@example.com");
-        loginPage.enterPassword("password");
+        loginPage.enterUserName(userName);
+        loginPage.enterPassword(password);
 
         dashboardPage = loginPage.tapOnLoginButton();
         MobileElement hamburgerElement = dashboardPage.getHamburgerMenu();
